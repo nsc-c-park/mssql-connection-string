83a3919e0b1ba6ae0c10654f6d35658da00cf7a7
Jacek Czastkiewicz <jacek.czastkiewicz@capgemini.com>
2017-04-15 19:04:44 +0200

    eslint issues fixed

--- test/knexSetupTest.js
+++ test/knexSetupTest.js
@@ -3,7 +3,7 @@ const parser = require('../index');
 
 describe('#knexSetup', () => {
     it('should return knex setup', () => {
-        const connectionString = 'Data Source=tcp:database.com,1433;Initial Catalog=numbers;User Id=service@database.com;Password=fjsflregewbfldsfhsew3;'
+        const connectionString = 'Data Source=tcp:database.com,1433;Initial Catalog=numbers;User Id=service@database.com;Password=fjsflregewbfldsfhsew3;';
         const expectedSetup = {
             'host': 'database.com',
             'options': {
