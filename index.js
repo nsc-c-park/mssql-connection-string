4e520c69c339fd0deb4b1007296967af3f7385e5
Jacek Czastkiewicz <jacek.czastkiewicz@capgemini.com>
2018-07-23 23:12:19 +0200

    [code-smells] refactoring

--- index.js
+++ index.js
@@ -7,56 +7,16 @@ const parse = require('./grammar.peg.js').parse;
  * @return {json}
  */
 module.exports = function (connectionString) {
-    let parts = parse(connectionString);
-    const result = parts.reduce((a, p)=> {
-        a[p[0].toLowerCase()] = p[1];
-        return a;
-    }, {});
+    const result = parseConnectionString(connectionString);
 
     // extract host and port from 'Data Source'
     let host;
     let port;
-    const dataSource = result['data source'];
-    if (dataSource) {
-        const regexFull = /.*:(.*)/;
-        const regexHostPort = /(.*),([0-9]+)/;
-        const matchFull = regexFull.exec(dataSource);
-        
-        if (matchFull) {
-            const matchHostPort1 = regexHostPort.exec(matchFull[1]);
-            if (matchHostPort1) {
-                host = matchHostPort1[1];
-                port = matchHostPort1[2];
-            }
-            else if (isNaN(matchFull[1])) {
-                
-                host = matchFull[1];
-            }
-        }
-        else{
-            const matchHostPort2 = regexHostPort.exec(dataSource);
-            if (matchHostPort2) {    
-                host = matchHostPort2[1];
-                port = matchHostPort2[2];
-            }
-            else if (isNaN(dataSource)) {
-                host = dataSource;
-            }
-        }
-    }
+    ({ host, port } = extractFromDataSource(result));
 
     // extract user from 'User Id'
     let user;
-    const userId = result['user id'];
-    if (userId) {
-        const regex = /(.*)@.*/;
-        const match = regex.exec(userId);
-        if (match) {
-            user = match[1];
-        } else {
-            user = userId;
-        }
-    }
+    user = extractFromUserId(result);
 
     // check if all data was found
     if (!host) {
@@ -78,9 +38,7 @@ module.exports = function (connectionString) {
             database: result['initial catalog'],
             encrypt: true
         },
-        /* tslint:disable */
         password: result['password'],
-        /* tslint:enable */
         user,
     };
 
@@ -90,3 +48,60 @@ module.exports = function (connectionString) {
 
     return config;
 };
+
+function extractFromUserId(result) {
+    let user;
+    const userId = result['user id'];
+    if (userId) {
+        const regex = /(.*)@.*/;
+        const match = regex.exec(userId);
+        if (match) {
+            user = match[1];
+        }
+        else {
+            user = userId;
+        }
+    }
+    return user;
+}
+
+function extractFromDataSource(result) {
+    let host;
+    let port;
+    const dataSource = result['data source'];
+    if (dataSource) {
+        const regexFull = /.*:(.*)/;
+        const regexHostPort = /(.*),([0-9]+)/;
+        const matchFull = regexFull.exec(dataSource);
+        if (matchFull) {
+            const matchHostPort1 = regexHostPort.exec(matchFull[1]);
+            if (matchHostPort1) {
+                host = matchHostPort1[1];
+                port = matchHostPort1[2];
+            }
+            else if (isNaN(matchFull[1])) {
+                host = matchFull[1];
+            }
+        }
+        else {
+            const matchHostPort2 = regexHostPort.exec(dataSource);
+            if (matchHostPort2) {
+                host = matchHostPort2[1];
+                port = matchHostPort2[2];
+            }
+            else if (isNaN(dataSource)) {
+                host = dataSource;
+            }
+        }
+    }
+    return { host, port };
+}
+
+function parseConnectionString(connectionString) {
+    let parts = parse(connectionString);
+    const result = parts.reduce((a, p) => {
+        a[p[0].toLowerCase()] = p[1];
+        return a;
+    }, {});
+    return result;
+}
