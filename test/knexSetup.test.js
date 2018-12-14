60f06cd58f30e05812a2283d7b23abb009ff3a47
Jacek <jc@archlab.pl>
2018-12-14 19:09:20 +0100

    Experimental/smells (#11)
    * [code-smells] refactoring
    
    * [code-smells] refactoring
    
    * [type-definition] refactoring, src files moved to lib
    
    * [type-definition] ignore for *.peg.js improved
    
    * [type-definition] src moved to src
    
    * fixing code smells
    
    * dos2unix
    
    * peg added to ignore

--- test/knexSetup.test.js
+++ test/knexSetup.test.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-const parser = require('../index');
+const parser = require('../src/index');
 
 describe('#knexSetup', () => {
     it('should return knex setup', () => {
